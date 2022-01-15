795c981dde10e69f651f1c570947d4a5583d0e23
Eduardo Assis <eduardohenriqueassis1973@gmail.com>
2022-01-15 13:33:45 -0300

    Finalização da página inicial

--- script/script.js
+++ script/script.js
@@ -1,29 +1,29 @@
 // btn mobile
 
-const btnMobile = document.getElementById("btn-mobile");
+const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
-  if (event.type === "touchstart") {
+  if (event.type === 'touchstart') {
     event.preventDefault();
   }
-  const nav = document.querySelector(".menu");
-  nav.classList.toggle("visible");
-  const visible = nav.classList.contains("visible");
-  event.currentTarget.setAttribute("aria-expanded", visible);
+  const nav = document.querySelector('.menu');
+  nav.classList.toggle('visible');
+  const visible = nav.classList.contains('visible');
+  event.currentTarget.setAttribute('aria-expanded', visible);
   if (visible) {
-    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
   } else {
-    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
   }
 }
 
-btnMobile.addEventListener("click", toggleMenu);
-btnMobile.addEventListener("touchstart", toggleMenu);
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
 
 // Set div slider´s height
-let sliderHeight = document.querySelector(".slider");
-let imgSlide = document.querySelector(".img-slide");
-let carouselUm = document.querySelector(".carousel-1");
+let sliderHeight = document.querySelector('.slider');
+let imgSlide = document.querySelector('.img-slide');
+let carouselUm = document.querySelector('.carousel-1');
 
 function resizeSlider() {
   sliderHeight.style.height = `${imgSlide.clientHeight}px`;
@@ -32,30 +32,30 @@ function resizeSlider() {
 }
 resizeSlider();
 
-document.addEventListener("scroll", resizeSlider);
-window.addEventListener("resize", resizeSlider);
+document.addEventListener('scroll', resizeSlider);
+window.addEventListener('resize', resizeSlider);
 
 // ------------ Slider ------------
-var slides = document.querySelectorAll(".slide");
-var btns = document.querySelectorAll(".btn");
+var slides = document.querySelectorAll('.slide');
+var btns = document.querySelectorAll('.btn');
 
 let currentSlide = 1;
 
 // Javascript for image slider manual navigation.
 var manualNav = function (manual) {
   slides.forEach((slide) => {
-    slide.classList.remove("active2");
+    slide.classList.remove('active2');
 
     btns.forEach((btn) => {
-      btn.classList.remove("active2");
+      btn.classList.remove('active2');
     });
   });
-  slides[manual].classList.add("active2");
-  btns[manual].classList.add("active2");
+  slides[manual].classList.add('active2');
+  btns[manual].classList.add('active2');
 };
 
 btns.forEach((btn, i) => {
-  btn.addEventListener("click", () => {
+  btn.addEventListener('click', () => {
     manualNav(i);
     currentSlide = i;
   });
@@ -64,17 +64,17 @@ btns.forEach((btn, i) => {
 //  Javascript for slide image autoplay
 
 var repeat = function (activeClass) {
-  let active = document.getElementsByClassName("active2");
+  let active = document.getElementsByClassName('active2');
   let i = 1;
 
   var repeater = () => {
     setTimeout(function () {
       [...active].forEach((activeSlide) => {
-        activeSlide.classList.remove("active2");
+        activeSlide.classList.remove('active2');
       });
 
-      slides[i].classList.add("active2");
-      btns[i].classList.add("active2");
+      slides[i].classList.add('active2');
+      btns[i].classList.add('active2');
       i++;
 
       if (slides.length == i) {
@@ -94,76 +94,74 @@ repeat();
 
 // Carousel products
 
-const prev = document.querySelector(".prev");
+const prev = document.querySelector('.prev');
 
-const next = document.querySelector(".next");
+const next = document.querySelector('.next');
 
-const track = document.querySelector(".track");
+const track = document.querySelector('.track');
 
-const card = document.querySelector(".card");
+const card = document.querySelector('.card');
 
-const bodyColor = document.querySelector("body");
+const bodyColor = document.querySelector('body');
 
-let windowWidth = window.innerWidth;
-
-let carouselWidth = document.querySelector(".carousel-inner").clientWidth;
+let carouselWidth = document.querySelector('.carousel-inner').clientWidth;
 
 let index = 0;
 
-window.addEventListener("resize", () => {
-  carouselWidth = document.querySelector(".carousel-container").offsetWidth;
+window.addEventListener('resize', adjustItemsOnResize);
+window.addEventListener('resize', showBtnOnResize);
+
+// adjustItemsOnResize();
+function adjustItemsOnResize() {
+  carouselWidth = document.querySelector('.carousel-container').offsetWidth;
   track.style.transform = `translateX(-${index * carouselWidth}px)`;
 
-  // console.log(carouselWidth);
-
-  adjustItemsOnResize();
-  function adjustItemsOnResize() {
-    if (carouselWidth >= 789 && index > 3) {
-      index = 3;
-     } else if (carouselWidth >= 1120 && index >= 3) {
-       index = 2;
-    }
+  if (carouselWidth >= 789 && carouselWidth < 1000 && index > 3) {
+    index = 3;
+  } else if (carouselWidth >= 1000 && index > 2) {
+    index = 2;
   }
+  console.log(carouselWidth, window.innerWidth);
+}
 
-  showBtnOnResize();
+// showBtnOnResize();
 
-  function showBtnOnResize() {
-    if (carouselWidth < 790 && index === 5) {
-      next.classList.add("hide");  
-
-    }else if (carouselWidth < 790 && index === 3) {
-      next.classList.remove("hide");
-    }
-     else if (carouselWidth < 1002 && index === 2) {
-      next.classList.remove("hide");
-    }
-     else if (carouselWidth >= 1002 && index === 2) {
-      next.classList.add("hide");
-    }
+function showBtnOnResize() {
+  if (carouselWidth < 790 && index === 5) {
+    next.classList.add('hide');
+  } else if (carouselWidth < 790 && index < 5) {
+    next.classList.remove('hide');
+  } else if (carouselWidth < 1000 && index === 3) {
+    next.classList.add('hide');
+  } else if (carouselWidth < 1000 && index === 2) {
+    next.classList.remove('hide');
+  } else if (carouselWidth >= 1000 && index === 2) {
+    next.classList.add('hide');
   }
-  // console.log('oi ' + index)
-});
+  console.log('oi ' + index);
+}
 
-next.addEventListener("click", () => {
+next.addEventListener('click', () => {
   index++;
-  prev.classList.add("show");
+  prev.classList.add('show');
   track.style.transform = `translateX(-${index * carouselWidth}px)`;
   if (carouselWidth > 1002 && index === 2) {
-    next.classList.add("hide");
-  } else if (carouselWidth >= 790 && index === 3) {
-    next.classList.add("hide");
-  }  else if (carouselWidth < 790 && index === 5) {
-    next.classList.add("hide");
-    
+    next.classList.add('hide');
+  } else if (carouselWidth < 790 && index >= 5) {
+    next.classList.add('hide');
+  } else if (carouselWidth < 790 && index < 5) {
+    next.classList.remove('hide');
+  } else if (carouselWidth <= 1002 && index === 3) {
+    next.classList.add('hide');
   }
   // console.log(index)
 });
 
-prev.addEventListener("click", () => {
+prev.addEventListener('click', () => {
   index--;
-  next.classList.remove("hide");
+  next.classList.remove('hide');
   if (index === 0) {
-    prev.classList.remove("show");
+    prev.classList.remove('show');
   }
   track.style.transform = `translateX(-${index * carouselWidth}px)`;
   // console.log(index);
